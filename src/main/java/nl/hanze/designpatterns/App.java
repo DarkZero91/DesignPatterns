722853fb8d029eb502516daeb0a6e71ff93d6811
Jeroen Hoekstra <jeroenhoekstra_1991@hotmail.com>
2015-09-19 14:34:46 +0200

    Adds client class

--- src/main/java/nl/hanze/designpatterns/App.java
+++ src/main/java/nl/hanze/designpatterns/App.java
@@ -1,5 +1,10 @@
 package nl.hanze.designpatterns;
 
+import nl.hanze.designpatterns.pizza.Pizza;
+import nl.hanze.designpatterns.pizza.PlainPizza;
+import nl.hanze.designpatterns.toppings.Mozzarella;
+import nl.hanze.designpatterns.toppings.TomatoSauce;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Pizza pizza = new Mozzarella(new TomatoSauce(new PlainPizza()));
+        System.out.println("Ingredients: " + pizza.getDescription());
+        System.out.println("Price: " + pizza.getPrice());
     }
 }
