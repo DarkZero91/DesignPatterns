5b397a3be4539de3d4366ab850546d885834722f
Jeroen Hoekstra <jeroenhoekstra_1991@hotmail.com>
2015-10-03 15:36:40 +0200

    Removes unused import

--- src/test/java/nl/hanze/designpatterns/FileDataHandlerTest.java
+++ src/test/java/nl/hanze/designpatterns/FileDataHandlerTest.java
@@ -2,7 +2,6 @@ package nl.hanze.designpatterns;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.io.FileWriter;
